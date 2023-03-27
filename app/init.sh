c2a0c2f4bd29586252f076cb45982199842afa2b
Evgeny Varnavskiy <varnavruz@gmail.com>
2023-03-27 19:45:45 -0400

    Fix database file download

--- app/init.sh
+++ app/init.sh
@@ -1,6 +1,6 @@
 # If used outside of Docker
-
-curl -s -o BaseStation.sqb.tar.gz https://github.com/varnav/BaseStation.sqb/releases/download/latest/BaseStation.sqb.tar.gz
+set -ex
+curl -s -L -o BaseStation.sqb.tar.gz https://github.com/varnav/BaseStation.sqb/releases/download/latest/BaseStation.sqb.tar.gz
 tar xf BaseStation.sqb.tar.gz
 rm -f BaseStation.sqb.tar.gz
 poetry install
