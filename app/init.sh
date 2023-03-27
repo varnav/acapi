aff65878b145ecfe6b9d91ce846ac8cf83676008
Evgeny Varnavskiy <varnavruz@gmail.com>
2023-03-27 18:59:47 -0400

    Add poentry install

--- app/init.sh
+++ app/init.sh
@@ -3,4 +3,5 @@
 curl -s -o BaseStation.sqb.tar.xz https://github.com/varnav/BaseStation.sqb/releases/download/latest/BaseStation.sqb.tar.xz
 tar xf BaseStation.sqb.tar.xz
 rm -f BaseStation.sqb.tar.xz
+poetry install
 python -m pytest /app
